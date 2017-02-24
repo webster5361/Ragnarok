a6261777dc0f5a602c31e8ad97d46fbbad960dc0
Odinthewanderer <webster5361@gmail.com>
2017-02-24 00:10:58 -0600

    Corrected all lint errors to bring it into a "No issues" state for the Travis CI Build.

--- commands/item/remove.js
+++ commands/item/remove.js
@@ -1,54 +1,45 @@
-const {
-  Command
-} = require('discord.js-commando');
+const { Command } = require('discord.js-commando');
 const Item = require('../../postgreSQL/models/Item');
 const Store = require('../../currency/Store');
-const StoreItem = require('../../currency/StoreItem');
 
 module.exports = class ItemAddCommand extends Command {
-  constructor(client) {
-    super(client, {
-      name: 'item-remove',
-      aliases: ['remove-item'],
-      group: 'item',
-      memberName: 'remove',
-      description: 'Removes an item to the store.',
-      throttling: {
-        details: 'Removes an item to the store.',
-        usages: 2,
-        duration: 3
-      },
+	constructor(client) {
+		super(client, {
+			name: 'item-remove',
+			aliases: ['remove-item'],
+			group: 'item',
+			memberName: 'remove',
+			description: 'Removes an item to the store.',
+			throttling: {
+				details: 'Removes an item to the store.',
+				usages: 2,
+				duration: 3
+			},
 
-      args: [{
-        key: 'name',
-        prompt: 'what item should be removed?\n',
-        type: 'string'
-      }]
-    });
-  }
+			args: [{
+				key: 'name',
+				prompt: 'what item should be removed?\n',
+				type: 'string'
+			}]
+		});
+	}
 
-  hasPermission(msg) {
-    return this.client.isOwner(msg.author);
-  }
+	hasPermission(msg) {
+		return this.client.isOwner(msg.author);
+	}
 
-  async run(msg, args) {
-    const name = args.name.toLowerCase();
-    const item = Store.getItem(name);
+	async run(msg, args) {
+		const name = args.name.toLowerCase();
+		const item = Store.getItem(name);
 
-    if (!item) return msg.reply('That item does not exist!');
+		if (!item) return msg.reply('That item does not exist!');
 
-    Store.getItems().delete(name);
+		Store.getItems().delete(name);
 
-    return Item.sync()
-      .then(() => {
-        Item.destroy({
-          where: {
-            name: item.name
-          }
-        });
-
-        return msg.say(
-          `The item **${name}** has been deleted, ${msg.author}`);
-      });
-  };
+		return Item.sync().then(() => {
+			Item.destroy({ where: { name: item.name } });
+			return msg.say(
+				`The item **${name}** has been deleted, ${msg.author}`);
+		});
+	}
 };
