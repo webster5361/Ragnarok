b3566ae808d49056529cdc255bba109a32e85b37
Odinthewanderer <webster5361@gmail.com>
2017-02-23 22:40:16 -0600

    Implemted remove command to remove an item from the store.

--- currency/Store.js
+++ currency/Store.js
@@ -1,4 +1,6 @@
-const { Collection } = require('discord.js');
+const {
+  Collection
+} = require('discord.js');
 
 const Item = require('../postgreSQL/models/Item');
 const StoreItem = require('./StoreItem');
@@ -6,21 +8,22 @@ const StoreItem = require('./StoreItem');
 const storeItems = new Collection();
 
 class Store {
-	static registerItem(item) {
-		storeItems.set(item.name, item);
-	}
+  static registerItem(item) {
+    storeItems.set(item.name, item);
+  }
 
-	static getItem(itemName) {
-		return storeItems.get(itemName);
-	}
+  static getItem(itemName) {
+    return storeItems.get(itemName);
+  }
 
-	static getItems() {
-		return storeItems;
-	}
+  static getItems() {
+    return storeItems;
+  }
 }
 
 Item.findAll().then(items => {
-	for (const item of items) Store.registerItem(new StoreItem(item.name, item.price));
+  for (const item of items) Store.registerItem(new StoreItem(item.name,
+    item.price));
 });
 
 module.exports = Store;
